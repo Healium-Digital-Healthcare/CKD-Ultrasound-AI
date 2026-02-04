8f8a2b4db0f26048f6f1c46f0f81b27134412e3b
Dereje <derejeseifu1991@gmail.com>
2026-02-04 09:07:08 +0300

    image added to report generator

--- store/services/ai.ts
+++ store/services/ai.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const AIApi = createApi({
   reducerPath: "aiApi",
+  // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1" }),
   baseQuery: fetchBaseQuery({ baseUrl: "https://ckd-ai.duckdns.org/api/v1" }),
   tagTypes: ["AI"],
   endpoints: (builder) => ({
